cbe62f896115d169108b09fbb544e89eddd598eb
lucky.li <wxindfx@gmail.com>
2019-10-26 10:48:30 +0800

    init;

---
+++ TestPropertyWrapper/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  TestPropertyWrapper
+//
+//  Created by lucky.li on 2019/10/26.
+//  Copyright © 2019 cico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+//        self.test()
+    }
+
+//    func test(_ value: Foo = Foo()) {
+//      let _: Codable = value
+//    }
+}
+
